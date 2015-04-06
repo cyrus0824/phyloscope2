6b5f07eec481b1956c8ba4332a2d629d0d880e6a
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2015-04-06 11:59:57 -0700

    Added some external js libraries.

--- src/widget/phyloscope2.js
+++ src/widget/phyloscope2.js
@@ -1,10 +1,10 @@
-/* Phylogenetic tree jquery widget */
-
-jQuery(function() {
-    jQuery.widget("phyloscope2.tree", {
+(function(jQuery) {
+    jQuery.widget("phylotree", {
         options: {
             lineHeight: 0,
             branchThickness: 0,
+            inputFormat: 'newick',
+            treeString: '',
             // Callback functions for events
             nodeMouseOver: null,
             nodeMouseOut: null,
@@ -21,17 +21,60 @@ jQuery(function() {
         // widget constructor
         _create: function() {
             // instance private variables
-            this.shit = "balls";
+            
+            // get JSON object of tree
+            if(this.options.inputFormat=='newick') {this.JSONtree = this.newickParse(this.options.treeString)}
+            else if(this.options.inputFormat=='phyloxml') {}
+            else {
+                // don't understand the format
+            }
+
+            console.log(this.JSONtree);
+            this.refresh()
         },
 
         _refresh: function() {
-
         },
 
         _destroy: function() {
 
         },
-        
+
+        _newickParse: function(s) {
+            // newick string parser courtesy of Jason Davies (newick.js) at
+            // https://github.com/jasondavies/newick.js
+            var ancestors = [];
+            var tree = {};
+            var tokens = s.split(/\s*(;|\(|\)|,|:)\s*/);
+            for (var i=0; i<tokens.length; i++) {
+              var token = tokens[i];
+              switch (token) {
+                case '(': // new branchset
+                  var subtree = {};
+                  tree.branchset = [subtree];
+                  ancestors.push(tree);
+                  tree = subtree;
+                  break;
+                case ',': // another branch
+                  var subtree = {};
+                  ancestors[ancestors.length-1].branchset.push(subtree);
+                  tree = subtree;
+                  break;
+                case ')': // optional name next
+                  tree = ancestors.pop();
+                  break;
+                case ':': // optional length next
+                  break;
+                default:
+                  var x = tokens[i-1];
+                  if (x == ')' || x == '(' || x == ',') {
+                    tree.name = token;
+                  } else if (x == ':') {
+                    tree.length = parseFloat(token);
+                  }
+              }
+            }
+            return tree;
+        }
     });
-
-
+}(jQuery));
