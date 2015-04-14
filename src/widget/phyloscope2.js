723f86a7c8d17f4e17a9ef71e99483306707fc4c
Cyrus Afrasiabi <cyrus.afrasiabi@gmail.com>
2015-04-13 21:31:10 -0700

    Added the annotation pull from uniprot.

--- src/widget/phyloscope2.js
+++ src/widget/phyloscope2.js
@@ -5,6 +5,9 @@ jQuery(function() {
             branchThickness: 0,
             inputFormat: 'newick',
             treeString: '',
+            // which columns should we annotate?
+            //annotationColumns: "columns=id,entry name,ec,go,go-id,genes,length,taxon,organism,organism-id,pathway,protein names,reviewed,version",            
+            annotationColumns: "columns=citation,clusters,comments,domains,domain,ec,id,entry name,existence,families,features,genes,go,go-id,interpro,interactor,keywords,last-modified,length,organism,organism-id,pathway,protein names,reviewed,sequence,3d,taxon,tools,version,virus hosts",
             // Callback functions for events
             nodeMouseOver: null,
             nodeMouseOut: null,
@@ -20,16 +23,18 @@ jQuery(function() {
 
         // widget constructor
         _create: function() {
-            // instance private variables
-            
             // get JSON object of tree
             if(this.options.inputFormat=='newick') {this.JSONtree = this._newickParse(this.options.treeString)}
             else if(this.options.inputFormat=='phyloxml') {}
             else {
                 // don't understand the format
             }
+            
 
-            console.log(this.JSONtree);
+            // get the root of the tree structure
+            this.tree = this._createTree(this.JSONtree);
+
+            //console.log(this);
             this._refresh();
         },
 
@@ -40,7 +45,74 @@ jQuery(function() {
 
         },
         _createTree: function() {
+            // function that turns the parsed json object into 
+            // the internal tree structure
+            var tree = {};
+            var that = this;            
+            var unpackJSONtree = function(node) {
+                var thisNode = {};
+                thisNode.children = [];
+                
+                // get this node's length and name, if applicable
+                if(node.hasOwnProperty('length')) {thisNode.distanceToParent = parseFloat(node['length']);}
+                if(node.hasOwnProperty('name')) {thisNode.name = node['name'];}
+                // recursively call this function
+                if(node.hasOwnProperty('branchset')) {
+                    for(var i=0; len=node['branchset'].length, i<len; i++) {thisNode.children.push(unpackJSONtree(node['branchset'][i]));}
+                }
+                return thisNode;
+            };
 
+            tree.root = unpackJSONtree(this.JSONtree);
+            
+            // traverses this tree and applies l to every node
+            tree.postOrderTraversal = function(l) {
+                var f = function(node) {
+                    for(var i=0; len=node.children.length, i<len; i++) {f(node.children[i]);}
+                    l(node);
+                };
+                f(tree.root);
+            };
+
+            // annotate one node
+            var annotateNode = function(node) {
+                // try to see what format this identifier is
+                if(node.hasOwnProperty('name')) {
+                    node.annotations = {};
+                    // is this a uniprot accession?
+                    var uniprotAccession1 = new RegExp("[OPQ][0-9][A-Z0-9][A-Z0-9][A-Z0-9][0-9]");
+                    var uniprotAccession2 = new RegExp("[A-NR-Z][0-9][A-Z][A-Z0-9][A-Z0-9][0-9]");
+                    var uniprotAccession3 = new RegExp("[A-NR-Z][0-9][A-Z][A-Z0-9][A-Z0-9][0-9][A-Z0-9][A-Z0-9][A-Z0-9][0-9]");
+                    var swissprotIdentifier = new RegExp("[A-Z0-9]{1,5}_[A-Z0-9]{1,5}");
+                    var thisQueryObject = {};
+                    if(uniprotAccession1.exec(node.name)) {thisQueryObject = {'query': uniprotAccession1.exec(node.name)[0], 'term': 'accession'};}
+                    else if(uniprotAccession2.exec(node.name)) {thisQueryObject = {'query': uniprotAccession2.exec(node.name)[0], 'term':'accession'};}
+                    else if(uniprotAccession3.exec(node.name)) {thisQueryObject = {'query': uniprotAccession3.exec(node.name)[0], 'term':'accession'};}
+                    else if(swissprotIdentifier.exec(node.name)) {thisQueryObject = {'query': swissprotIdentifier.exec(node.name)[0], 'term': 'mnemonic'};}
+                    // can't find an annotation
+                    else {return null;}
+                    // try to get annotations for this node
+                    jQuery.get("http://www.uniprot.org/uniprot/?query=" + thisQueryObject.term + ":" + thisQueryObject.query + "&" + that.options.annotationColumns + "&format=tab&limit=1", function(data) {
+                        // callback with data for this node
+                        if(data.split("\n").length>=2) {
+                            var thisKeyLine = data.split("\n")[0].split("\t");
+                            var thisAnnotationLine = data.split("\n")[1].split("\t");
+                            for(var i=0; i<thisKeyLine.length; i++) {node.annotations[thisKeyLine[i]] = thisAnnotationLine[i];}
+                        }
+                        else node.annotations = {}
+                    });
+                }
+            }; 
+            
+            // annotate tree
+            tree.postOrderTraversal(annotateNode);
+
+            tree.drawTree = function() {
+                // function that draws this tree
+            };
+            tree.drawTree();
+            console.log(tree);
+            return tree; 
         },
 
         _newickParse: function(s) {
